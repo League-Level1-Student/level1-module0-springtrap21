12ea0616f640bdf78c6e4f14601072345965982b
ilabstaff <ilabstaff@172.16.116.69>
2019-10-02 19:26:27 -0700

    houseMouse

--- src/_01_houses/houses.java
+++ src/_01_houses/houses.java
@@ -9,21 +9,39 @@ public class houses {
 	public static void main(String[] args) {
 		rob.setX(10);
 		rob.setY(500);
-		MakeHouse(120);
-		MakeHouse(250);
-		MakeHouse(80);
-		MakeHouse(250);
-		MakeHouse(120);
-		MakeHouse(80);
+		MakeHouse("medium" , Color.red);
+		MakeHouse("large" , Color.blue);
+		MakeHouse("small" , Color.PINK);
+		MakeHouse("large" , Color.blue);
+		MakeHouse("medium" , Color.PINK);
+		MakeHouse("small" , Color.red);
+		MakeHouse("large" , Color.PINK);
+		MakeHouse("small" , Color.red);
+		MakeHouse("medium" , Color.	blue);
+		
+		
 	}
 	
-	public static void MakeHouse(int height) {
+	public static void MakeHouse(String height2 , Color boi) {
+		int height = 0;
+		if (height2.equals("small")) {
+			height = 80;
+		}
+		else if (height2.equals("medium")) {
+			height = 120;
+		}
+		else if (height2.equals("large")){
+			height = 250;
+		}
+		rob.setPenColor(boi);
 		rob.penDown();
-		rob.setSpeed(5);
+		rob.setSpeed(50);
 		rob.move(height);
+		rob.turn(45);
+		rob.move(30);
 		rob.turn(90);
-		rob.move(45);
-		rob.turn(90);
+		rob.move(30);
+		rob.turn(45);
 		rob.move(height);
 		rob.turn(-90);
 		rob.setPenColor(Color.green);
